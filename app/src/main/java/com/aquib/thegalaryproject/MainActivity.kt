8b4bb8f4a9362a2ccd46b87671e631af8965ff4e
shaikhaquib <aquib.shaikh@baseinformation.com>
2021-06-05 01:30:53 +0530

    Task

---
+++ app/src/main/java/com/aquib/thegalaryproject/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.aquib.thegalaryproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    public fun registerClick(v:View){
+        startActivity(Intent(applicationContext,DashBoard::class.java))
+    }
+}
